e3e6f12ba56471c7c3a3d523ae1edc80e59f1b74
付恒吉 <649942163>
2023-06-14 09:55:49 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+
+function App() {
+  return (
+      <Fragment>
+          <div className="App">
+              <h1>This is React App.</h1>
+          </div>
+          <div>
+              <h1>与上面的标签处于一个并列的状态</h1>
+          </div>
+      </Fragment>
+  )
+}
+
+export default App
